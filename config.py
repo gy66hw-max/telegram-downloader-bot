56d81c62db8a0f863d28eb49807c171ec1789439
gy66hw-max <gy66hw@gmail.com>
2026-08-19 18:47:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # الإعدادات الأساسية والمعرفات
-BOT_TOKEN = "8835004502:AAEPbeFYj73N6APogOSCYGj0_ui05Kx0Ijo"
+BOT_TOKEN = "8835004502:AAGst61dtrophACdbmv3MInX-bx-qfe19gg"
 ADMIN_ID = 6269140922
 REWARDS_CHANNEL = "https://t.me/ishaqrvg"
 DB_NAME = "bot_database.db"
 
 # أسعار الاشتراكات بالعملات (Coins)
 SUB_PRICES = {
-    "daily": 25,     # يومي
-    "weekly": 75,    # أسبوعي
-    "monthly": 250   # شهري
+    "daily": 10,     # يومي
+    "weekly": 20,    # أسبوعي
+    "monthly": 100   # شهري
 }
 DEV_USERNAME = "ishaqrv"  # ضع يوزرك هنا بدون @
